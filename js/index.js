5b16c760519eefc7d9617cadaac8be8f99675c6c
Yurij Lishchuk <yurijlishchuk@gmail.com>
2018-08-01 15:01:56 +0000

    iitial

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// for (let i=0;i<50;i++) {
+//     document.write((50-i)/(1/2)+'<br>');
+// }
+
+// let i=1;
+// while (i<=100) {
+//     document.write(i+'<br>');
+//     i++;
+// }
+
+let productNames = ['Tesla X','BMW MX 6','Toyota Landcruiser 200', 'Mercedes GLE', 'Ford Edge', 'Chevrolet Tahoe'];
